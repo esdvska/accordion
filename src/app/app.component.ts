ab5b8f1c8858c1942886c13fc04ae1e70e449315
AzureAD\EwelinaSadowska <ewelina.sadowska@bmpconsulting.pl>
2024-06-14 12:04:06 +0200

    accordion problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  DxAccordionModule,
+  DxListComponent,
+  DxListModule,
+} from 'devextreme-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DxAccordionModule, DxListModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  @ViewChild(DxListComponent, { static: false }) assignList!: DxListComponent;
+
+  @ViewChild(DxListComponent, { static: false })
+  documentStatusesList!: DxListComponent;
+
   title = 'accordion';
+
+  documentAssignedFilters = [
+    { id: 0, text: 'Assigned to to me' },
+    { id: 1, text: 'Assigned' },
+    { id: 2, text: 'Unassigned' },
+  ];
+  documentStatuses = [
+    { id: 0, text: 'Status 0' },
+    { id: 1, text: 'Status 1' },
+    { id: 2, text: 'Status 2' },
+  ];
+  selectedAssignedKeys = [{ id: 1, text: 'Assigned' }];
+  selectedStatusKeys = [{ id: 0, text: 'Status 0' }];
+
+  ngAfterViewInit() {
+    this.assignList.instance.selectItem(this.selectedAssignedKeys[0].id);
+    this.documentStatusesList.instance.selectItem(
+      this.selectedStatusKeys[0].id
+    );
+  }
 }
